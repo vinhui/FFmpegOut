70c63cae45ced651c4996ec88a307abfcea57796
vinhui <vinni.vinni.vin@gmail.com>
2020-08-22 17:30:19 +0200

    Created enum for compression presets

---
+++ Assets/FFmpegOut/Runtime/CompressionSpeed.cs
@@ -0,0 +1,16 @@
+namespace FFmpegOut
+{
+    // https://trac.ffmpeg.org/wiki/Encode/H.264#a2.Chooseapresetandtune
+    public enum CompressionSpeed
+    {
+        UltraFast,
+        SuperFast,
+        VeryFast,
+        Faster,
+        Fast,
+        Medium,
+        Slow,
+        Slower,
+        VerySlow
+    }
+}
